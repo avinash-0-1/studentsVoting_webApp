c1dca4c386b195487a9859e9e1869bc24a3fce9c
avinash <avinashraj0918@gmail.com>
2026-03-12 00:05:49 +0530

    created login route

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,7 +1,36 @@
 import express from 'express'
 import db from '../database.js'
+import userModel from '../models/userSchema.js'
 
 const route = express.Router()
+
+route.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const user = await userModel.findOne({ username: username })
+
+        if (!username) {
+            return res.send('Invalid username')
+        }
+        const isMatchPass = user.comparePass(password)
+        if (!isMatchPass) {
+            return res.send('Invalid Password')
+        }
+
+        const payload = {
+            id: user.id,
+            username: user.username
+        }
+
+        const token = generateToken(payload)
+        res.status(200).json({ token })
+    } catch (error) {
+        res.status(500).json({ message: 'Server Side ERROR' })
+    }
+
+})
+
+
 
 
 
---
+++ jwt.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+//generate token
+
+const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.jwtkey)
+}
+
+//verification of token
+const jwtMiddleware = (req, res, next) => {
+    const authentication = req.headers.authorization
+    if (!authentication) {
+        return res.status(401).json({ message: "Unauthorized" })
+    }
+
+    const bearerToken = req.headers.authorization.split(' ')[1]
+    if (!bearerToken) {
+        return res.status(401).json({ message: 'Token Not Found' })
+    }
+
+    try {
+        const encodedPayload = jwt.verify(bearerToken, process.env.jwtkey)
+        req.user = encodedPayload
+        next()
+    } catch (error) {
+        res.status(500).json({ message: 'server side ERROR' })
+    }
+}
+
+export { generateToken , jwtMiddleware }
