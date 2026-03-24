d5b7facc96000a4681c18a4d55b9dea4ba846e10
avinash <avinashraj0918@gmail.com>
2026-03-24 18:41:12 +0530

    implemented voting logic

--- Routes/candidatesRoute.js
+++ Routes/candidatesRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import candidateModel from '../models/candidateSchema.js'
 import userModel from '../models/userSchema.js';
 import { jwtMiddleware } from '../jwt.js';
+import { use } from 'react';
 
 
 const candidateRoute = express.Router()
@@ -10,23 +11,23 @@ const candidateRoute = express.Router()
 const checkRoleForAdmin = async (userId) => {
     const user = await userModel.findById(userId)
     console.log("USER ROLE:", user.role)
-     if (!user){ return false}
+    if (!user) { return false }
     return user.role === "admin";
 }
 
 //creating new candidate for election & save it into the DB
-candidateRoute.post('/candidatepost',jwtMiddleware, async (req, res) => {
+candidateRoute.post('/candidatepost', jwtMiddleware, async (req, res) => {
     try {
         const { name, party, age } = req.body; // for secure the candidates records by securing the isVoted field so that isVoted can't be manipulated
 
         if (!(await checkRoleForAdmin(req.user.id))) {
             return res.status(403).json({ message: "you're not the admin !!" })
         }
-        
-        const newData = new candidateModel({name, party, age })
+
+        const newData = new candidateModel({ name, party, age })
         const response = await newData.save()
-        
-        res.json({ response:response , message: 'new Candidate Added !' })
+
+        res.json({ response: response, message: 'new Candidate Added !' })
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Server Side ERROR" })
@@ -69,6 +70,44 @@ candidateRoute.delete('/candidatedelete:id', async (req, res) => {
         res.status(500).json({ message: "server side ERROR" })
     }
 })
+//======================================= VOTING LOGIC ==================================
 
+candidateRoute.post('vote/candidateID', async (req, res) => {
+    const candidateID = req.params.id;
+    const userID = req.user.id;
+
+    try {
+        const candidate = await candidateModel.findById(candidateID)
+        if (!candidate) {
+            return res.status(404).json({ message: "candidate not found !" })
+        }
+        const user = await userModel.findById(userID)
+        if (!user) {
+            return res.status(404).json({ message: "user not found !" })
+        }
+        if (user.role == "admin") {
+            return res.status(403).json({ message: "Admin are Not Allowed to vote" })
+        }
+        if (user.isVoted) {
+            return res.status(400).json({ message: "You have already voted" })
+        }
+
+        //update candidate to record vote.
+        candidate.vote.push({ user: userID })
+        candidate.voteCount++;
+        await candidate.save()
+
+        //update voters record.
+        user.isVoted = true;
+        await user.save()
+
+        res.status(200).json({ message: "Vote is Recorded Successfully !!" })
+
+    } catch (error) {
+        res.status(500).json({ message: "Server side ERROR" })
+    }
+})
+
+//================ 
 
 export default candidateRoute
