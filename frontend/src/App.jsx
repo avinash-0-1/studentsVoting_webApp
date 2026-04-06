49e199331669320a7bb958fde9038a2d941a4299
user.name "avinash <avinash>
2026-04-06 15:49:09 +0530

    set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login.jsx";
+import Vote from "./pages/vote.jsx";
+import Results from "./pages/result.jsx";
+import PrivateRoute from "./components/privateRoute.jsx";
+import Admin from "./pages/admin.jsx";
+
+
+
