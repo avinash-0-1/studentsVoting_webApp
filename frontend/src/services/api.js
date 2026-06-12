38d7d3d139566022db3bf1ce9e1d082bbe864932
user.name "avinash <avinash>
2026-06-12 17:04:23 +0530

    solved url related issues

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://studentsvoting-webapp.onrender.com", //https://studentsvoting-webapp.onrender.com
-                                                          //http://localhost:3000/
+                                                         //http://localhost:3000/
 });
 
 API.interceptors.request.use((req) => {
