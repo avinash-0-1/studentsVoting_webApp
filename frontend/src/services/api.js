72e096d60803c00a378ea4a7b8459aaa9505d85b
user.name "avinash <avinash>
2026-06-12 16:41:13 +0530

    minor changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://studentsvoting-webapp.onrender.com", //https://studentsvoting-webapp.onrender.com
+                                                          //http://localhost:3000/
 });
 
 API.interceptors.request.use((req) => {
