becb07c13977b5b60e38655c4f57e676eb97bd1b
user.name "avinash <avinash>
2026-06-12 16:30:01 +0530

    created _redirects to handle routes issue in netlify

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://studentsvoting-webapp.onrender.com",
+  baseURL: "http://localhost:3000/",
 });
 
 API.interceptors.request.use((req) => {
